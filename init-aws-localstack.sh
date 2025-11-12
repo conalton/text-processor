3244c729812ad969eba1dad84da28c02841c29be
Stanislav.Khamitov <stanislav.khamitov@statista.com>
2025-11-12 23:54:41 +0100

    Update init-aws-localstack.sh script

--- init-aws-localstack.sh
+++ init-aws-localstack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker-compose exec -it localstack awslocal s3 mb s3://text-cleaner-uploads --region eu-central-1
-docker-compose exec -it localstack awslocal sqs create-queue --queue-name text-cleaner-tasks --region eu-central-1
+docker-compose exec -it localstack awslocal s3 mb s3://text-processor-uploads --region eu-central-1
+docker-compose exec -it localstack awslocal sqs create-queue --queue-name text-processor-tasks --region eu-central-1
