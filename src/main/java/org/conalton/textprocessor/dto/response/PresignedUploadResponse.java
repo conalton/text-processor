9c4318c9e7862d9839afc70a02ce595fdaba086f
Stanislav.Khamitov <stanislav.khamitov@statista.com>
2025-11-13 01:56:46 +0100

    fix: PresignedUploadResponse should implement ApiResponse

--- src/main/java/org/conalton/textprocessor/dto/response/PresignedUploadResponse.java
+++ src/main/java/org/conalton/textprocessor/dto/response/PresignedUploadResponse.java
@@ -1,3 +1,5 @@
 package org.conalton.textprocessor.dto.response;
 
-public record PresignedUploadResponse(String taskId, String uploadUrl) {}
+import org.conalton.textprocessor.dto.response.common.ApiResponse;
+
+public record PresignedUploadResponse(String taskId, String uploadUrl) implements ApiResponse {}
