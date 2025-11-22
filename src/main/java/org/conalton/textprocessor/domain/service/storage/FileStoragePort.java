765a3352c0501a6908b9badc6b8290ece34c976c
Stanislav.Khamitov <stanislav.khamitov@statista.com>
2025-11-22 02:10:51 +0100

    refactor: reorganize packages and naming conventions

--- src/main/java/org/conalton/textprocessor/domain/service/storage/FileStoragePort.java
+++
@@ -1,7 +0,0 @@
-package org.conalton.textprocessor.domain.service.storage;
-
-import org.conalton.textprocessor.dto.internal.PresignedUrlData;
-
-public interface FileStoragePort {
-  PresignedUrlData generatePresignedUploadUrl(StorageLocation location, String uploadPath);
-}
